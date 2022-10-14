059806f1d654ca452b15bce90b3eed77454e6658
Ryan Borges <ryanborgeslockser@gmail.com>
2022-10-13 22:05:40 -0300

    alterações

--- src/br/edu/ufersa/hospital/model/service/MedicoBO.java
+++ src/br/edu/ufersa/hospital/model/service/MedicoBO.java
@@ -1,9 +1,30 @@
 package br.edu.ufersa.hospital.model.service;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import br.edu.ufersa.hospital.model.entity.Medico;
 import br.edu.ufersa.hospital.model.dao.MedicoDAO;
 import br.edu.ufersa.hospital.model.dao.BaseInterDAO;
 
 public class MedicoBO {
+
+    /*  IBaseInterDAO<Medico> dao = new MedicoDAO();
+	public boolean adicionar(Medico med) {
+		ResultSet rs = dao.funcaoPChamar
+		try {
+			if(rs==null || !(rs.next())) {
+				if(dao.editar(med) == true)
+					return true;
+					else return false;
+			}
+			else return false;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}	
+	} */
 
 public void cadastrar(Medico med){ // Melhorar cadastrar() com a implementação do banco de dados
     med.setNome(med.getNome()); // Esses sets provavelmente vão pro BD
@@ -26,15 +47,17 @@ public void excluir(Medico med){
 }
 
 public Medico buscarPorCodigo(Medico med){ // "Medico med" vai ter o med.buscar == codConselho ou cpf buscado
-    if (this.codigoDoConselho == med.codigoDoConselho){
-        return Medico.this; // Retorna a própria classe
-    } else return med; // Alterar esse else?
+    //if (this.codigoDoConselho == med.codigoDoConselho){
+    //    return Medico.this; // Retorna a própria classe
+    //} else return med; // Alterar esse else?
+    return med;
 }
 
 public Medico buscarPorCpf(Medico med){ // "Medico med" vai ter o med.buscar == codConselho ou cpf buscado
-    if (this.cpf == med.cpf){
-        return Medico.this; // Retorna a própria classe
-    } else return med; // Alterar esse else?
+    //if (this.cpf == med.cpf){
+    //  return Medico.this; // Retorna a própria classe
+    //} else return med; // Alterar esse else
+    return med;
 }
 
 }
