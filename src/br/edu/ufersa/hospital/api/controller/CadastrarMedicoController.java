8a450087904a2e7369804aae1a779bf0bff34593
Ryan Borges <ryanborgeslockser@gmail.com>
2022-11-13 22:29:40 -0300

    add: telas cadastro e adaptações

--- src/br/edu/ufersa/hospital/api/controller/CadastrarMedicoController.java
+++ src/br/edu/ufersa/hospital/api/controller/CadastrarMedicoController.java
@@ -7,14 +7,20 @@ import br.edu.ufersa.hospital.model.service.MedicoBO;
 
 public class CadastrarMedicoController {
     
-    @FXML private TextField login;
-    @FXML private TextField senha;
+    @FXML private TextField nome;
+    @FXML private TextField cpf;
+    @FXML private TextField endereco;
+    @FXML private TextField codConselho;
+    @FXML private TextField valorConsulta;
     private MedicoBO bo = new MedicoBO();
     
     public void cadastrar() {
         MedicoDTO dto = new MedicoDTO();
-        dto.setNome(login.getText());
-        dto.setCpf(senha.getText());
+        dto.setNome(nome.getText());
+        dto.setCpf(cpf.getText());
+        dto.setEndereco(endereco.getText());
+        //dto.setCodigoDoConselho(codConselho.getText());
+        //dto.setValorDaConsulta(valorConsulta.getText());
         bo.adicionar(dto);
         // Telas.listarMedicos();   método ainda não implementado
     }
